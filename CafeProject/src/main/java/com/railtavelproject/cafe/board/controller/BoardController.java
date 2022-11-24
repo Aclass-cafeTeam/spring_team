4aa1029a8a2ae3c59bbf6157d36c94592ca4b975
rkdalsrb65 <rkdalsrb65@naver.com>
2022-11-24 14:35:01 +0900

    Board

--- CafeProject/src/main/java/com/railtavelproject/cafe/board/controller/BoardController.java
+++ CafeProject/src/main/java/com/railtavelproject/cafe/board/controller/BoardController.java
@@ -1,5 +1,28 @@
 package com.railtavelproject.cafe.board.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.railtavelproject.cafe.board.model.service.BoardService;
+
+@Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService service;
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
