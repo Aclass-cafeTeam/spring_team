7763629c9cc4d3b629fdcd16c03316b0b094d274
rkdalsrb65 <rkdalsrb65@naver.com>
2022-11-24 14:48:44 +0900

    BoardService

--- CafeProject/src/main/java/com/railtavelproject/cafe/board/model/service/BoardService.java
+++ CafeProject/src/main/java/com/railtavelproject/cafe/board/model/service/BoardService.java
@@ -1,6 +1,14 @@
 package com.railtavelproject.cafe.board.model.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface BoardService {
+
+	/** 게시판 이름 목록 조회
+	 * @return boardTypeList
+	 */
+	List<Map<String, Object>> selectBoardType();
 
 	
 	
