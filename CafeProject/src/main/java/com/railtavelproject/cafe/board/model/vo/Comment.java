89579938e46ae72f4dc809e8f00b57d056c61bee
jihoon-git <hooon.h@gmail.com>
2022-12-04 23:48:32 +0900

    Comment VO 추가

--- CafeProject/src/main/java/com/railtavelproject/cafe/board/model/vo/Comment.java
+++ CafeProject/src/main/java/com/railtavelproject/cafe/board/model/vo/Comment.java
@@ -1,5 +1,14 @@
 package com.railtavelproject.cafe.board.model.vo;
 
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class Comment {
 	   private int commentNo; // 댓글 번호   COMMENT_NO
 	   private String commentContent; // 댓글 내용   COMMENT_CONTENT
