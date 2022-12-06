8d3427546e23ea55eb352c662bffead25a8218a6
jihoon-git <hooon.h@gmail.com>
2022-12-06 17:32:03 +0900

    좋아요 기능 복구

--- CafeProject/src/main/java/com/railtavelproject/cafe/board/controller/BoardController.java
+++ CafeProject/src/main/java/com/railtavelproject/cafe/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.railtavelproject.cafe.board.model.service.BoardDetailService;
 import com.railtavelproject.cafe.board.model.service.BoardService;
@@ -16,6 +17,7 @@ import com.railtavelproject.cafe.board.model.vo.BoardType;
 import com.railtavelproject.cafe.member.model.vo.Member;
 
 @Controller
+@SessionAttributes({"boardInfo"})
 public class BoardController {
 
 	@Autowired
