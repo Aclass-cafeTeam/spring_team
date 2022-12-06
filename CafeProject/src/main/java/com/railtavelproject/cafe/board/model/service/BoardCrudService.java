a999e5a7b31094e6d5a0c3c7cc5f3e9fa90fa5f4
Sseohee <108666056+SseoheeKim@users.noreply.github.com>
2022-12-07 01:41:26 +0900

    12/7 마이페이지 삭제글 태그명 명시 수정, 게시글 삭제 변경

--- CafeProject/src/main/java/com/railtavelproject/cafe/board/model/service/BoardCrudService.java
+++ CafeProject/src/main/java/com/railtavelproject/cafe/board/model/service/BoardCrudService.java
@@ -3,14 +3,17 @@ package com.railtavelproject.cafe.board.model.service;
 import java.util.List;
 
 import java.util.Map;
-
-import org.springframework.web.multipart.MultipartFile;
-
 import com.railtavelproject.cafe.board.model.vo.Board;
 import com.railtavelproject.cafe.member.model.vo.Member;
 
 public interface BoardCrudService {
-
+	
+	/** 게시판 목록 조회(로그인한 회원 등급에 따라)
+	 * @param memberLevelNo
+	 * @return boardTypeList
+	 */
+	List<Map<String, Object>> selectBoardType(Member loginMember);
+	
 	/** 태그 조회
 	 * @return titleTagList
 	 */
@@ -69,11 +72,7 @@ public interface BoardCrudService {
 	int deleteTempAll(int memberNo);
 
 
-	/** 게시판 목록 조회(로그인한 회원 등급에 따라)
-	 * @param memberLevelNo
-	 * @return boardTypeList
-	 */
-	List<Map<String, Object>> selectBoardType(Member loginMember);
+	
 
 
 }
